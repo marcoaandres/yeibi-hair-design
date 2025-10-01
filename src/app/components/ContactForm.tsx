e46ed310fa529bcf1477d1d57f44f09bdf40b260
Marco Andres <marcoa@gluo.mx>
2025-09-30 18:15:38 -0600

    Add. validation for date in calendar

--- src/app/components/ContactForm.tsx
+++ src/app/components/ContactForm.tsx
@@ -3,13 +3,15 @@
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import emailjs from "@emailjs/browser"
-import DatePicker from "react-datepicker";
+import DatePicker, { registerLocale } from "react-datepicker";
+import { es } from 'date-fns/locale/es';
 
 import { CalendarIcon } from "lucide-react";
 import { Button } from "./ui";
 
 import 'react-datepicker/dist/react-datepicker.css';
 
+registerLocale('es', es)
 
 export const ContactForm = () => {
     const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
@@ -56,6 +58,9 @@ export const ContactForm = () => {
         email: Yup.string().email('El correo electrónico es inválido')
         .required('El correo electrónico es obligatorio'),
         service: Yup.string().required('El servicio es obligatorio'),
+        date: Yup.date().required('La fecha es obligatoria')
+        .min(minDate, 'La fecha no puede ser anterior a hoy')
+        .max(maxDate, 'La fecha no puede ser mayor a un mes a partir de hoy'),
         message: Yup.string().min(10, 'El mensaje debe tener al menos 10 caracteres')
         .max(150, 'El mensaje es muy largo, por favor sé más breve'),
     }),
@@ -167,8 +172,10 @@ export const ContactForm = () => {
           dateFormat={"dd/MM/yyyy"}
           showIcon
           icon={<CalendarIcon />}
+          locale="es"
           className="file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border px-3 py-1 text-base bg-input-background transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive"
         />
+        {(typeof formik.errors.date === "string" && formik.touched.date) && (<p className="text-sm text-destructive">{formik.errors.date}</p>)}
       </div>
       <div className="space-y-2">
         <label
